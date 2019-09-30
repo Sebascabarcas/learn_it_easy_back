5caa0722c4efb909d54cf733db42c5245ac073b1
Sebastian Cabarcas <sebascabarcas@gmail.com>
2019-09-30 14:57:55 -0500

    delete of devise configurations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,4 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
   include FieldFormatter
   #Enums
   enum role: { student: 0, teacher: 1 }
