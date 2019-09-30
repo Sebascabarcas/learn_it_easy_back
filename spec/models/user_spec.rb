03bea3482b52cd23d11d6d3fb5c6c031324d6684
Sebastian Cabarcas <sebascabarcas@gmail.com>
2019-09-30 15:03:23 -0500

    user_spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,16 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
-
   describe 'validations' do
     it 'validate presence of required fields' do
       should validate_presence_of(:email)
-      should validate_presence_of(:password)
+      # should validate_presence_of(:password)
       should validate_presence_of(:role)
-      should validate_presence_of(:first_name)
-      should validate_presence_of(:second_name)
-      should validate_presence_of(:first_latname)
+      should validate_presence_of(:name)
+      should validate_presence_of(:first_lastname)
       should validate_presence_of(:second_lastname)
     end
   end
